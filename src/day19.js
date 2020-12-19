02f5b78e3aa080435ccee9b583f5532b9d97fddd
Julián Absatz <julianabsatz@gmail.com>
2020-12-19 19:15:48 +0100

    cleanup day 19

--- src/day19.js
+++ src/day19.js
@@ -6,7 +6,9 @@ const parse = input => {
   const rules = _.fromPairs(
     rawRules.map(line => {
       const [key, rawRule] = line.split(': ');
-      const rule = rawRule.startsWith('"') ? rawRule.replace(/\"/g, '') : rawRule.split(' ');
+      const rule = rawRule.startsWith('"')
+        ? rawRule.replace(/\"/g, '')
+        : rawRule.split(' | ').map(rules => rules.split(' '));
       return [key, rule];
     }),
   );
@@ -15,82 +17,53 @@ const parse = input => {
 };
 
 class MonsterRegex {
-  constructor(rules, options) {
+  constructor(rules) {
     this.rules = rules;
-    this.shouldLog = options.log;
-    this.potentialChecks = [];
   }
 
-  log(message) {
-    if (this.shouldLog) {
-      process.stdout.write(`\n${message}`);
-    }
-  }
-
-  test(message) {
-    this.log(`new message: ${message}`);
+  test = message => {
     const messagesLeft = this.testAgainst(message, ['0']);
     const isValid = messagesLeft.some(message => message.length === 0);
-    this.log(`${message} is ${isValid ? 'valid' : 'invalid'}!`);
-    this.log('\n\n');
     return isValid;
-  }
+  };
 
-  testAgainst(initialMessage, keys) {
+  testAgainst = (initialMessage, keys) => {
     const key = _.last(keys);
     const currentRule = this.rules[key];
 
-    if (initialMessage.length === 0) return [];
+    if (initialMessage.length === 0) return []; // nothing can match an empty message
 
     if (typeof currentRule === 'string') {
       return initialMessage.startsWith(currentRule) ? [initialMessage.substring(currentRule.length)] : [];
     }
 
-    this.log(`testing against rule ${key} (${currentRule.join(' ')}), path [${keys}]`);
-
-    let messagesToCheck = [initialMessage];
-    let messagesPassed = [];
-    for (let i = 0; i < currentRule.length; i++) {
-      const char = currentRule[i];
-
-      const newMessagesLeft = [];
-
-      if (char === '|') {
-        messagesPassed = messagesToCheck;
-        messagesToCheck = [initialMessage];
-      } else {
-        messagesToCheck.forEach(message => {
-          const validMessages = this.testAgainst(message, [...keys, char]);
-
-          validMessages.forEach(validMessage => {
-            newMessagesLeft.push(validMessage);
-          });
-        });
-        messagesToCheck = newMessagesLeft;
-      }
-    }
-    messagesPassed = [...messagesPassed, ...messagesToCheck];
-    this.log(`testing against rule ${key} finished, new possible messages: [${messagesPassed}]`);
+    const messagesPassed = currentRule.flatMap(ruleSet => {
+      return ruleSet.reduce(
+        (messagesToCheck, char) => {
+          return messagesToCheck.flatMap(message => this.testAgainst(message, [...keys, char]));
+        },
+        [initialMessage],
+      );
+    });
 
     return messagesPassed;
-  }
+  };
 }
 
 const part1 = input => {
   const { rules, messages } = parse(input);
 
-  const regexTester = new MonsterRegex(rules, { log: false });
+  const regexTester = new MonsterRegex(rules);
 
   return messages.filter(message => regexTester.test(message)).length;
 };
 
 const part2 = input => {
-  const { rules, messages } = parse(input);
+  const { rules, messages } = parse(
+    input.replace('8: 42', '8: 42 | 42 8').replace('11: 42 31', '11: 42 31 | 42 11 31'),
+  );
 
-  rules['8'] = ['42', '|', '42', '8'];
-  rules['11'] = ['42', '31', '|', '42', '11', '31'];
-
-  const regexTester = new MonsterRegex(rules, { log: false });
+  const regexTester = new MonsterRegex(rules);
 
   return messages.filter(message => regexTester.test(message)).length;
 };
